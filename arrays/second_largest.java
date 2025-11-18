12fb9ab45048a7d12c89d9916298353eb23f6dd9
shoyebchaudhari <you@example.com>
2025-11-19 00:14:58 +0530

    Second largest element solved  with some coditions changed

--- arrays/second_largest.java
+++ arrays/second_largest.java
@@ -1,33 +1,37 @@
 package arrays;
 import java.util.Scanner;
+
 public class second_largest {
-public static void main(String args[]){
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter the Size of the Array ");
-int n = sc.nextInt();
-int arr [] = new int[n];
-System.out.println("Enter the Elements of the Array ");
-for(int i =0;i<n;i++){
-    arr[i]=sc.nextInt();
-}
+    public static void main(String[] args) {
 
-int largest =-1;
-int secondl = arr[0];
-for(int i = 1;i<n;i++){
-    if(arr[i]<largest && arr[i]>secondl){
-        secondl=arr[i];
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter the Size of the Array ");
+        int n = sc.nextInt();
 
-    } else if(arr[i]>largest){
-        secondl= largest;
-        largest= arr[i];
+        int arr[] = new int[n];
 
+        System.out.println("Enter the Elements of the Array ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
 
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;  // previous largest becomes second
+                largest = arr[i];
+            }
+
+            else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Second Largest Element is: " + secondLargest);
+        sc.close();
     }
-     
-} 
-System.out.println("Second Largest element "+secondl);
-
-}
-    
 }
